141537d65fd44138a3c77f8b384dd51fdf0912eb
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-05-19 23:01:01 +0530

    changed the files and logic

--- script.js
+++ script.js
@@ -1,32 +1,9 @@
-const textInput = document.getElementById('textInput');
-const fontDisplay = document.getElementById('fontDisplay');
-const fontFileInput = document.getElementById('fontFileInput');
-
-textInput.addEventListener('input', function() {
-    fontDisplay.textContent = this.value;
-});
-
-fontFileInput.addEventListener('change', function() {
-    loadFontFromFile(this.files[0]);
-});
-
-function loadFontFromFile(file) {
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const fontData = e.target.result;
-            const fontFormat = file.name.split('.').pop(); // Get the file extension
-            const fontUrl = `data:font/${fontFormat};base64,${fontData}`;
-            // Create and apply @font-face rule dynamically
-            const fontFace = `@font-face {
-                font-family: 'uploadedFont';
-                src: url('${fontUrl}') format('${fontFormat}');
-            }`;
-            const style = document.createElement('style');
-            style.innerHTML = fontFace;
-            document.head.appendChild(style);
-            fontDisplay.style.fontFamily = 'uploadedFont';
-        };
-        reader.readAsDataURL(file);
-    }
+function changeFontSize() {
+    const fontSizeInput = document.querySelector('.input[oninput="changeFontSize()"]');
+    const contentElement = document.getElementById('content');
+  
+    const fontSize = fontSizeInput.value.replace(/\D/g, '');
+    const fontUnit = fontSizeInput.value.replace(/\d/g, '') || 'pt';
+  
+    contentElement.style.fontSize = `${fontSize}${fontUnit}`;
 }
--- test/script.js
+++
@@ -1,9 +0,0 @@
-function changeFontSize() {
-    const fontSizeInput = document.querySelector('.input[oninput="changeFontSize()"]');
-    const contentElement = document.getElementById('content');
-  
-    const fontSize = fontSizeInput.value.replace(/\D/g, '');
-    const fontUnit = fontSizeInput.value.replace(/\d/g, '') || 'pt';
-  
-    contentElement.style.fontSize = `${fontSize}${fontUnit}`;
-}
